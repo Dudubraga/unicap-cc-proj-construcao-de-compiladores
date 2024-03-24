c85ed846725383b6041cf0b43c2841d857503835
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-03-24 17:28:11 -0300

    analisador lexico

--- analisador_lexico/main.py
+++ analisador_lexico/main.py
@@ -1,1 +1,17 @@
-#teste
+palavras_reservadas = ["int", "float", "while", "for", "char", "main", "return", 
+                        "boolean", "if", "else", "elif", "scanf", "println", 
+                        "printf", "return", "void", "str", "string"]
+
+operadores = ["=", "+", "-", "/", "*", "%", "&&", "||", "!"]
+
+operadores_comparacao = [">", ">=", "<", "<=", "==", "!="]
+
+simbolos_especiais = ["(", ")", ",", "[", "]", "{", "}", ";"]
+
+lista_tokens = []
+NUM_INT = []
+NUM_DEC = []
+const_txt = []
+identificadores = []
+
+# nem ideia
