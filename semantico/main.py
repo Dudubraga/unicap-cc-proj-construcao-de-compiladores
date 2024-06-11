32662ade6a82631c34f1e0c9099780728e56d3f7
HenriqueFrancaa <henrique23.franca@gmail.com>
2024-06-10 23:58:53 -0300

    compila a dor

--- semantico/main.py
+++ semantico/main.py
@@ -1,21 +1,20 @@
-import rascunho_semantico as semantic
+import semantico as semantico
 from lexer import lexico
 from parser import sintatico
 
 
-def semantico():
+def compilador():
     data = '''
         float a; 
         a = 2;
         int b;
         b = 1;
         boolean validate;
-        validate = 2;
+        validate = True;
         float d; 
         d = (2+2)/2;
     '''
     lista = data.split('\n')
-
 
     lexico.input(data)
     for token in lexico:
@@ -25,8 +24,8 @@ def semantico():
 
     print(result)
     
-    if semantic.analisar(data):
+    if semantico.analisar(data):
         print("Análise semântica feito com sucesso!")
 
 if __name__ == "__main__":
-    semantico()
+    compilador()
--- semantico/rascunho_semantico.py
+++ semantico/rascunho_semantico.py
@@ -1,61 +1,82 @@
-class Variavel:
-    def __init__(self, nome, tipo):
-        self.nome = nome
-        self.tipo = tipo
 
-variaveis = []
+inteiros = {}
+flutuantes = {}
+booleanos = {}
+operadores = {'+', '-', '*', '/'}
 
-def add_variavel(nome, tipo):
-    variaveis.append(Variavel(nome, tipo))
-
-def verificar_variavel(nome):
-    for variavel in variaveis:
-        if variavel.nome == nome:
-            return True
-    return False
-
-def ver_atribuicao(tipo1, tipo2):
-    if tipo1 == 'int' and tipo2 == 'float':
-        return False
-    if tipo1 == 'boolean' and (tipo2 == 'float' or tipo2 == 'int'):
-        return False
-    if (tipo1 == 'float' or tipo2 == 'int') and tipo2 == 'boolean':
-        return False
+data = """
     
-    return True
+    int x = 10;
+    float z = 9.0;
+    int h = 12.23;
+    
+    2 + 3 * 2;
 
-def get_tipo(nome):
-    for variavel in variaveis:
-        if variavel.nome == nome:
-            return variavel.tipo
-    return None
+    """
+lista = data.split('\n')
 
-def analisar(data):
-    linhas = data.split('\n')
-    for i, linha in enumerate(linhas):
-        if 'int ' in linha or 'float ' in linha or 'boolean' in linha:
-            l = linha.split()
-            tipo = l[0]
-            nome = l[1].replace(';', '')
-            add_variavel(nome, tipo)
-        elif '=' in linha:
-            l = linha.split('=')
-            variavel = l[0].strip()
-            valor = l[1].strip().replace(';', '')
-            if not verificar_variavel(variavel):
-                print(f"Erro semântico: variável '{variavel}' não declarada na linha {i+1}")
-                return False
-            tipo_variavel = get_tipo(variavel)
-            if valor.isdigit():
-                tipo_valor = 'int'
-            elif valor.replace('.', '', 0).isdigit():
-                tipo_valor = 'float'
-            elif valor == 'True' or valor == 'False':
-                tipo_valor = 'boolean'
+for l in lista:
+    while len(lista) != 0:
+        current = str(l[0])
+        l = l[1:] #remove o primeiro caracter de l
+        for i in range(len(lista)):
+            if l[0] == ' ':
+                l = l[1:]
+                break;    
+            current += l[i]
+            l = l[1:]
+
+        tipo = '0'
+        if current == 'int':
+            tipo = 'int'
+        elif current == 'float':
+            tipo = 'float'
+        elif current == 'double':    
+            tipo = 'double'
+        elif current == 'boolean':    
+            tipo = 'boolean'
+        current = ''
+
+        while l[0] != ' ':
+            current += l[0]
+            l = l[1:]
+        l = l[1:]
+        if tipo != '0' and current in (inteiros or flutuantes or booleanos): 
+            print(f'variável {current} declarada novamente')
+            exit()
+        elif tipo == '0' and not (current in (inteiros or flutuantes or booleanos)):
+            print(f'variavel {current} não declarada')
+            exit()
+        elif tipo == 'float' or tipo == 'double':
+            flutuantes.append(current)
+
+        elif tipo == 'int':
+            inteiros.append(current)
+        
+        elif tipo == 'boolean':
+            booleanos.append(current)
+        
+        print(f"váriavel {current}, TIPO: {tipo}")
+
+        current = ''
+        current += l[0]
+        l = l[1:]
+
+        if current == ';':
+            break
+        elif current == '=':
+            current = ''
+            l = l[1:]
+            while l[0] != ' ':
+                current += l[0]
+                l = l[1:]
+            l = l[1:]
+            if current == 'True' or current == 'False':
+                tipo = 'boolean'
+            elif '.' in current:
+                tipo = 'float'
             else:
-                tipo_valor = 'expressao'
-            
-            if not ver_atribuicao(tipo_variavel, tipo_valor):
-                print(f"Erro semântico: atribuição de tipo incompatível para a variável '{variavel}' na linha {i+1}")
-                return False
-    return True
+                tipo = 'int'
+        elif current in operadores:
+            break
+
